2c60a9449c560c3c4257b945e719fc8e00ba63d1
serika12345 <281916063+serika12345@users.noreply.github.com>
2025-09-07 01:12:17 +0900

    TODO

--- src/components/ScreenMode.tsx
+++ src/components/ScreenMode.tsx
@@ -5,12 +5,13 @@ import { ScreenCanvas } from "./ScreenCanvas";
 export const ScreenMode: React.FC = () => {
     return (
         <>
+            {/* TODO: モーダル */}
             <div>
                 <button onClick={() => alert("未実装")}>スプライトを追加</button>
             </div>
 
             <>
-                {/* TODO: BGを描画する */}
+                {/* TODO: 配置されたスプライトを描画する */}
                 <ScreenCanvas scale={5} showGrid={true} />
             </>
         </>
