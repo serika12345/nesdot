c87d21b9e965ae8f9178e5cf21de7cad812f8993
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-03-25 21:05:48 +0900

    Add base URL configuration for GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 const host = process.env.TAURI_DEV_HOST;
+const githubRepository = process.env.GITHUB_REPOSITORY;
+const repositoryName =
+  typeof githubRepository === "string" ? githubRepository.split("/")[1] : "";
+const base =
+  process.env.GITHUB_ACTIONS === "true" && repositoryName.length > 0
+    ? `/${repositoryName}/`
+    : "/";
 
 export default defineConfig(() => ({
   plugins: [react({ jsxImportSource: "@emotion/react" })],
+  base,
   clearScreen: false,
   test: {
     environment: "node",
